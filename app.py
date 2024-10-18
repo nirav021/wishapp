c26a3864a76c39470891b60719133d83b253703b
nirav <anirav52@gmail.com>
2024-10-18 09:30:04 -0400

    Update Flask app to use environment variables

--- app.py
+++ app.py
@@ -1,26 +1,23 @@
 from flask import Flask, render_template
 from pymongo import MongoClient
+from dotenv import load_dotenv
 from urllib.parse import quote_plus
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
-# Replace with your actual MongoDB credentials
-username = "host"
-password = "host"
+# Get MongoDB username and password from environment variables
+username = os.getenv('MONGODB_USERNAME')
+password = os.getenv('MONGODB_PASSWORD')
 password = quote_plus(password)  # URL-encode your password if needed
 
 # MongoDB connection
 mongo_client = MongoClient(f"mongodb+srv://{username}:{password}@cluster0.per6r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = mongo_client["Wish"]
 collection = db["products"]
-
-# Uncomment this line if you want to insert mock data once
-#mock_data = [
-  #    {"name": "Polo t-Shirt", "tag": "new", "price": 13.99, "image_path": "/static/images/polotshirt.jpg"},
- #   {"name": "t-Shirt", "tag": "new", "price": 8.99, "image_path": "/static/images/tshirt.jpg"},
-#    {"name": "Shirt", "tag": "new", "price": 23.99, "image_path": "/static/images/shirt.jpg"}]
-
-#collection.insert_many(mock_data)  # Uncomment this to insert data once
 
 # Define routes
 @app.route("/")
